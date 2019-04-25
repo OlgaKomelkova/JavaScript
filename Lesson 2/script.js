9e45d08cc8eb227226ffde7d96ea0d8eef68ef88
OlgaKomelkova <47720988+OlgaKomelkova@users.noreply.github.com>
2019-04-25 23:20:33 +0500

    Add files via upload

--- Lesson 2/script.js
+++ Lesson 2/script.js
@@ -16,35 +16,57 @@ for (let i = 0; i < 2; i++) {
         b = prompt("Во сколько обойдется?", '');
 
     if ( (typeof(a))=== 'string' && (typeof(a)) !== null && (typeof (b)) !== null
-        && a !=='' && b !== '' && a.length < 50) {
+        && a !== '' && b !== '' && a.length < 50) {
         console.log("done");
         appData.expenses[a] = b;
+    } else if ( (typeof(a)) !== 'string' && (typeof(a))=== null && (typeof (b))=== null
+        && a=== '' && b=== '' && a.length > 50) {
     } else {
-
+        alert("Ошибка");   
+        i--;
     } 
 }
-	
-/*var i = 0
+
+    /*var i = 0;
 
 do {
   let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
       b = prompt("Во сколько обойдется?", '');
       appData.expenses[a] = b;
       i++;
+      if ( (typeof(a))=== 'string' && (typeof(a)) !== null && (typeof (b)) !== null
+        && a !== '' && b !== '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else if ( (typeof(a)) !== 'string' && (typeof(a))=== null && (typeof (b))=== null
+        && a=== '' && b=== '' && a.length > 50) {
+    } else {
+        alert("Ошибка");   
+        i--;
+    } 
 }
 
-while (i < 2)
-*/	
+    while (i < 2);*/	
 
-/*var i = 0
+/*var i = 0;
 
 while (i<2) {
 	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
       b = prompt("Во сколько обойдется?", '');
       appData.expenses[a] = b;
       i++;
+      if ( (typeof(a))=== 'string' && (typeof(a)) !== null && (typeof (b)) !== null
+        && a !== '' && b !== '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else if ( (typeof(a)) !== 'string' && (typeof(a))=== null && (typeof (b))=== null
+        && a=== '' && b=== '' && a.length > 50) {
+    } else {
+        alert("Ошибка");   
+        i--;
+    } 
 }
 */
 
 appData.moneyPerDay = appData.budget/30;
-alert (appData.moneyPerDay);
+alert(appData.moneyPerDay);
--- Lesson 2/tempCodeRunnerFile.js
+++ Lesson 2/tempCodeRunnerFile.js
@@ -1,19 +1,52 @@
- /*jshint esversion: 6 */
-let money = prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "");
-let article = prompt("Введите обязательную статью расходов в этом месяце", "");
-let costs = prompt("Во сколько обойдется?");
+/*jshint esversion: 6 */
+ let money = prompt("Ваш бюджет на месяц?", '');
+ let time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-let appData = {
-    budget: {money}, 
-    timeData: {time},
-    expenses: {
-        "article" : "costs"
-    },
-    optionalExpenses: {},
-    income: [],
-    savings: 'false',
-};
+ let appData = {
+     budget: money, 
+     timeData: time,
+     expenses: {},
+     optionalExpenses: {},
+     income: [],
+     savings: 'false',
+ };
+ 
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
 
+    if ( (typeof(a))=== 'string' && (typeof(a)) !== null && (typeof (b)) !== null
+        && a !=='' && b !== '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else if ((typeof(a))!== 'string' && (typeof(a)) === null && (typeof (b)) === null
+        && a =='' && b == '' && a.length > 50) {
+        alert("Ошибка");
+        continue;
+    } 
+}
+	
+/*var i = 0
 
+do {
+  let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+      b = prompt("Во сколько обойдется?", '');
+      appData.expenses[a] = b;
+      i++;
+}
 
+while (i < 2)
+*/	
+
+/*var i = 0
+
+while (i<2) {
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+      b = prompt("Во сколько обойдется?", '');
+      appData.expenses[a] = b;
+      i++;
+}
+*/
+
+appData.moneyPerDay = appData.budget/30;
+alert (appData.moneyPerDay);
