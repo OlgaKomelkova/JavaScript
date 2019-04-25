e95eac9da6f4291b7b5fe19634d577d1c079cc41
OlgaKomelkova <47720988+OlgaKomelkova@users.noreply.github.com>
2019-04-25 18:38:16 +0500

    Add files via upload

---
+++ Lesson 2/script.js
@@ -0,0 +1,50 @@
+ /*jshint esversion: 6 */
+ let money = prompt("Ваш бюджет на месяц?", '');
+ let time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+ let appData = {
+     budget: money, 
+     timeData: time,
+     expenses: {},
+     optionalExpenses: {},
+     income: [],
+     savings: 'false',
+ };
+ 
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+
+    if ( (typeof(a))=== 'string' && (typeof(a)) !== null && (typeof (b)) !== null
+        && a !=='' && b !== '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+
+    } 
+}
+	
+/*var i = 0
+
+do {
+  let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+      b = prompt("Во сколько обойдется?", '');
+      appData.expenses[a] = b;
+      i++;
+}
+
+while (i < 2)
+*/	
+
+/*var i = 0
+
+while (i<2) {
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+      b = prompt("Во сколько обойдется?", '');
+      appData.expenses[a] = b;
+      i++;
+}
+*/
+
+appData.moneyPerDay = appData.budget/30;
+alert (appData.moneyPerDay);
