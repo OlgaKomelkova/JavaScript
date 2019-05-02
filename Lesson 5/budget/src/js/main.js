3551536a3c56dbeb39420db410baca0de8ee3b0f
OlgaKomelkova <47720988+OlgaKomelkova@users.noreply.github.com>
2019-05-02 21:47:15 +0500

    Add files via upload

---
+++ Lesson 5/budget/src/js/main.js
@@ -0,0 +1,42 @@
+/*jshint esversion: 6 */
+
+let start = document.getElementById('start'),
+    budgetValue = document.querySelectorAll('.budget-value'),
+    dayBudgetValue = document.querySelectorAll('.daybudget-value'),
+    levelValue = document.querySelectorAll('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'), 
+    incomeValue = document.querySelector('.income-value'),
+    yearSavingsValue = document.querySelector('.yearsavings-value'),
+    expensesItem = document.querySelector('.expenses-item'),
+    buttonExpensesItem = document.querySelector('.expenses-item-btn'),
+    buttonOptionalExpenses = document.querySelector('.optionalexpenses-btn'),
+    buttonCount = document.querySelector('.count-budget-btn'),
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income'),
+    checkSavings = document.querySelectorAll('.checksavings'),
+    sum = document.querySelector('#sum'),
+    percent = document.querySelector('#percent'),
+    yearValue = document.querySelectorAll('.year-value'),
+    monthValue = document.querySelectorAll('.month-value'),
+    dayValue = document.querySelectorAll(".day-value");
+
+    console.log(start);
+    console.log(budgetValue);
+    console.log(dayBudgetValue);
+    console.log(levelValue);
+    console.log(expensesValue);
+    console.log(incomeValue);
+    console.log(yearSavingsValue);
+    console.log(expensesItem);
+    console.log(buttonExpensesItem);
+    console.log(buttonOptionalExpenses);
+    console.log(buttonCount);
+    console.log(optionalExpensesItem);
+    console.log(chooseIncome);
+    console.log(checkSavings);
+    console.log(percent);
+    console.log(sum);
+    console.log(yearValue);
+    console.log(monthValue);
+    console.log(dayValue);
+   
