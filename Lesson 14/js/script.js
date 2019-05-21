53e318889cfcdce4f7c1dbcc4bb0141cb68412fb
Olga <olgakomelkova@mail.ru>
2019-05-22 00:12:51 +0500

    Модальные окна

---
+++ Lesson 14/js/script.js
@@ -0,0 +1,20 @@
+ /*jshint esversion: 6 */
+ 
+ $(document).ready(function () {
+	$('[href="#sheldure"]').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideDown("slow");
+	});
+	$('.main_btn').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideDown("slow");
+	});
+	$('.main_btna').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideDown("slow");
+	});
+	$('.close').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideUp("slow");
+	});
+});
